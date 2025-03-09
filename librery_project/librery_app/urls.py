ae89d3b12f13085191b88bbeb6a3ce68acc6679b
Fabian Robayo <willyfaropaezg0809@gmail.com>
2025-03-09 15:51:38 -0500

    segundo comit actualizando urls

--- librery_project/librery_app/urls.py
+++ librery_project/librery_app/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 # Importa las vistas desde el módulo views de la aplicación actual
 from .views import (
     AutorList, CrearAutor,
-    TareaList, TareaByFecha, TareaByRangoFechas, TareaByPersona
+    EditorialList, CrearEditorial,
+    LibroList, CrearLibro,
+    MiembroList, CrearMiembro,
+    PrestamoList, CrearPrestamo,
 )
 
 # Define la lista de patrones de URL para la aplicación 'api_app'
@@ -14,4 +17,23 @@ urlpatterns = [
     # Crear una nueva persona
     path('autores/crear/', CrearAutor.as_view(), name='autor-crear'),
 
+    # Lista de personas
+    path('editoriales/', EditorialList.as_view(), name='editorial-list'),
+    # Crear una nueva persona
+    path('editoriales/crear/', CrearEditorial.as_view(), name='editorial-crear'),
+
+        # Lista de personas
+    path('libros/', LibroList.as_view(), name='libro-list'),
+    # Crear una nueva persona
+    path('libros/crear/', CrearLibro.as_view(), name='libro-crear'),
+
+        # Lista de personas
+    path('miembro/', MiembroList.as_view(), name='miembro-list'),
+    # Crear una nueva persona
+    path('miembro/crear/', CrearMiembro.as_view(), name='miembro-crear'),
+
+        # Lista de personas
+    path('prestamos/', PrestamoList.as_view(), name='prestamo-list'),
+    # Crear una nueva persona
+    path('prestamos/crear/', CrearPrestamo.as_view(), name='prestamo-crear'),
 ]
