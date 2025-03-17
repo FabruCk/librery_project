91b34e3ce8fd6adeae9ae687b165a997b5df191d
Fabian Robayo <willyfaropaez0809@gmail.com>
2025-03-17 09:10:29 -0500

    todo arreglado

--- librery_project/librery_project/settings.py
+++ librery_project/librery_project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'librery_db',
         'USER': 'postgres',
-        'PASSWORD': '123456789',
+        'PASSWORD': 'Negro08062006.',
         'HOST': 'localhost',
         'PORT': '5432'
     }
